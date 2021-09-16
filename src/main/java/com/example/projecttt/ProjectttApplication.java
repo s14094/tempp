09d5871fd386ad751f0a7665991a41c256c872e1
Paweł Łakomiec <pawellakomiec95@gmail.com>
2021-09-17 00:34:12 +0200

    first commit

---
+++ src/main/java/com/example/projecttt/ProjectttApplication.java
@@ -0,0 +1,13 @@
+package com.example.projecttt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectttApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectttApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projecttt/ProjectttApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projecttt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectttApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
